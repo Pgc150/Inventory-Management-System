bc049bf56da4093d0ae6f08e2c1981e262619a41
Payal Chavhan <payalchavhan180@gmail.com>
2026-02-09 14:46:35 +0530

    update in axios instance

--- ims-frontend/src/lib/axios.js
+++ ims-frontend/src/lib/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // backend base URL
-  withCredentials: true,                // IMPORTANT for cookies
+  baseURL: `${API_URL}/api`, 
+  withCredentials: true,               
 });
 
 export default axiosInstance;
 
 export const productInstance = axios.create({
-   baseURL:"http://localhost:5000/api",
+   baseURL:`${API_URL}/api`,
    withCredentials: true,
 })
